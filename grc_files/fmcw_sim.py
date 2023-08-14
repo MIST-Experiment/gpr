add07507bf60a6d458b4574fae5fd7cf90a6fa34
mikehetu <michael.hetu@mail.mcgill.ca>
2023-08-14 16:58:39 -0400

    Additional tests 07-12

--- grc_files/fmcw_sim.py
+++ grc_files/fmcw_sim.py
@@ -76,7 +76,7 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 12000000
+        self.samp_rate = samp_rate = 25000000
         self.center_freq = center_freq = 100000000
 
         ##################################################
@@ -204,15 +204,15 @@ class fmcw_sim(gr.top_block, Qt.QWidget):
         self.qtgui_sink_x_0_2.enable_rf_freq(False)
 
         self.top_layout.addWidget(self._qtgui_sink_x_0_2_win)
-        self.blocks_vco_f_0_0 = blocks.vco_f(samp_rate, 3140000*5, 1)
+        self.blocks_vco_f_0_0 = blocks.vco_f(samp_rate, 3140000*10, 1)
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.blocks_file_sink_0_0_2 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/gprlab/Desktop/gnu_log/chirp_log06', False)
+        self.blocks_file_sink_0_0_2 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/gprlab/Desktop/gnu_log/chirp_log14', False)
         self.blocks_file_sink_0_0_2.set_unbuffered(False)
-        self.blocks_file_sink_0_0_1 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/gprlab/Desktop/gnu_log/out1_log06', False)
+        self.blocks_file_sink_0_0_1 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/gprlab/Desktop/gnu_log/out1_log14', False)
         self.blocks_file_sink_0_0_1.set_unbuffered(False)
-        self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/gprlab/Desktop/gnu_log/out0_log06', False)
+        self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/gprlab/Desktop/gnu_log/out0_log14', False)
         self.blocks_file_sink_0_0_0.set_unbuffered(False)
-        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SAW_WAVE, 1000000, 1, 0, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SAW_WAVE, 10000, 1, 0, 0)
 
 
         ##################################################
